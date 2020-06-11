9b6cef9780081f532d8c66bfccb71494b1e03b52
ilkwonKIM <ilkim226@gmail.com>
2020-06-11 15:18:06 +0900

    banner거의 완성

--- js/index.js
+++ js/index.js
@@ -9,17 +9,54 @@ function b() {
 var fnA = a();
 var fnB = b();
 console.log(fnA, fnB);
+
+var css = {"position": "absolute", "top": "50%", "transform": "translateY(-50%)", "font-size": "5rem", "z-index": 900, "color": "#fff"};
+var $btLeft = $('<i class="bt-lt fa fa-angle-left"></i>').appendTo(".main-wrap").css(css);
+var $btRight = $('<i class="bt-rt fa fa-angle-right"></i>').appendTo(".main-wrap").css(css);
+$btLeft.css("left", "2rem");
+$btRight.css("right", "2rem");
 */
 
 /******************* 전역설정 ********************/
-mainSlide();
+mainSlide(".main-wrap");
 
 
 
 /******************* 사용자 함수 ********************/
-function mainSlide() {
-	var $slide = $(".main-wrap").find(".banner").remove();
-	console.log($slide);
+function mainSlide(container) {
+	var now = 0;
+	var $container = $(container).addClass("slide-wrap");
+	var $slide = $container.children("*").addClass("slide").css("transition", "0.5s");
+	var $btPrev = $('<div class="bt bt-prev"></div>').appendTo($container).click(onPrev);
+	var $btNext = $('<div class="bt bt-next"></div>').appendTo($container).click(onNext);
+	// console.log($slide);
+	var last = $slide.length - 1;
+
+	function init() {
+		$btPrev.show();
+		$btNext.show();
+		$container.children(".slide").remove();
+		$($slide[now]).appendTo($container);
+	}
+	function ani() {
+		$($slide[now]).appendTo($container).css({"opacity": 0, "transform": "scale(1.2)"});
+		setTimeout(function(){
+			$container.children(".slide").eq(0).css({"opacity": 0, "transform": "scale(0.7)"});
+			$container.children(".slide").eq(1).css({"opacity": 1, "transform": "scale(1)"});
+			setTimeout(init, 500);
+		}, 0);
+	}
+	function onPrev() {
+		$(this).hide();
+		now = (now == 0) ? now = last : now - 1;
+		ani();
+	}
+	function onNext() {
+		$(this).hide();
+		now = (now == 2) ? now = 0 : now + 1;
+		ani();
+	}
+	init();
 }
 
 
