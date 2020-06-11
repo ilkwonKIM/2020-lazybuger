03bd18de3c27b90c911bad870ae979333140c793
ilkwonKIM <ilkim226@gmail.com>
2020-06-11 11:28:31 +0900

    headerScroll

---
+++ js/index.js
@@ -0,0 +1,45 @@
+/********************* 사전지식 *******************/
+
+
+
+
+
+
+/********************* 전역설정 *******************/
+window.scTop = 0; 
+window.wid = 0; 
+window.hei = 0 
+
+
+
+
+
+
+/********************* 사용자 함수 *******************/
+
+
+
+
+
+
+
+/********************* 이벤트 함수 *******************/
+function onResize() {
+	this.wid = $(this).innerWidth();
+	this.wid = $(this).innerHeight();
+}
+function onScroll() {
+	this.scTop =	$(this).scrollTop();
+	if(this.scTop > this.hei) {
+		$(".header").css({"top": 0,"bottom":"auto","position":"fixed"})
+	}
+	else {
+		$(".header").css({"top": "auto","bottom":0,"position":"absolute"})
+	}
+}
+
+
+
+/********************* 이벤트 설정 *******************/
+$(window).resize(onResize).trigger("resize");
+$(window).scroll(onScroll).trigger("scroll");
